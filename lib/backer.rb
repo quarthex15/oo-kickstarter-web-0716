0900f908d6b6aaad668438e218fbcb77b3f89b44
Andrew Klein <andrew.klein.j@gmail.com>
2016-07-31 22:33:33 -0400

    complete assignment

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+
+  attr_accessor :backed_projects, :name
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    project.backers << self
+    @backed_projects << project
+  end
+
+
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    backer.backed_projects << self
+    @backers << backer
+  end
+
+
+end
